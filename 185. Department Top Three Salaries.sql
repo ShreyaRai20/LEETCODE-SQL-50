5416a7faa2e4cc60b473c5d68a9abfba96a3316d
shreya200199 <86977802+shreya200199@users.noreply.github.com>
2024-06-06 14:01:34 -0500

    Update 185. Department Top Three Salaries.sql

--- 185. Department Top Three Salaries.sql
+++ 185. Department Top Three Salaries.sql
@@ -5,3 +5,28 @@ JOIN
 FROM EMPLOYEE) AS E
 ON E.departmentId = D.id 
 WHERE R_salary <= 3;
+
+
+-- The above code is giving the right output with different order.
+--  To get the right order(order in LEETCODE) run the code given below.
+
+SELECT D.name AS Department, 
+       E.name AS Employee, 
+       E.salary AS Salary
+FROM Department D 
+JOIN Employee E ON E.departmentId = D.id
+WHERE E.salary IN (
+    SELECT DISTINCT salary
+    FROM (
+        SELECT salary,
+               departmentId,
+               DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS salary_rank
+        FROM Employee
+    ) AS RankedSalaries
+    WHERE salary_rank <= 3
+        AND RankedSalaries.departmentId = D.id
+)
+ORDER BY D.name, E.salary DESC;
+
+
+
