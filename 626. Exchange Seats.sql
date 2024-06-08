f4fec8d54e2bd780b75c8b103dfe5da9d7a7552d
shreya200199 <86977802+shreya200199@users.noreply.github.com>
2024-06-08 18:05:39 -0500

    Create 626. Exchange Seats.sql

---
+++ 626. Exchange Seats.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT (CASE WHEN ID%2 = 0 THEN ID-1
+WHEN ID%2 != 0 AND ID < (SELECT MAX(ID) FROM SEAT) THEN ID+1
+ELSE ID
+END) AS ID,
+student 
+FROM SEAT
+ORDER BY id;
