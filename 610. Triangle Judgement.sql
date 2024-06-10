e87291e93ca53bb78beb33cbd901453c6af97562
shreya200199 <86977802+shreya200199@users.noreply.github.com>
2024-06-10 16:34:21 -0500

    Update 610. Triangle Judgement.sql

--- 610. Triangle Judgement.sql
+++ 610. Triangle Judgement.sql
@@ -1,2 +1,54 @@
+/*
+610. Triangle Judgement
+Solved
+Easy
+
+Topics
+Companies
+SQL Schema
+Pandas Schema
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+*/
+
+-SOLUTION
+
+
 SELECT  x, y , z, if(x+y>z AND z+y>x AND x+z>y, 'Yes', 'No') triangle
 FROM Triangle;
